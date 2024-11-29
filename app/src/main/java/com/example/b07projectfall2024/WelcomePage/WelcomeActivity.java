42d2da7d4b7eb0b0152b322ed5a157b08c45cdec
AoxiangSun <aoxiang.sun@mail.utoronto.ca>
2024-11-29 06:54:12 -0500

    Update WelcomeActivity.java with correct import

--- app/src/main/java/com/example/b07projectfall2024/WelcomePage/WelcomeActivity.java
+++ app/src/main/java/com/example/b07projectfall2024/WelcomePage/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package com.example.b07projectjava;
+package com.example.b07projectfall2024.WelcomPagepackage;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+
+import com.example.b07projectfall2024.LoginPage.LoginActivityView;
+import com.example.b07projectfall2024.RegisterPage.RegisterActivityView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
